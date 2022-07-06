0375ac22f8e19b85d3201909a19a0a4425746075
KarlitosD <carlosdcampos.04@gmail.com>
2022-07-06 00:35:49 -0300

    cambiar comillas simples por backtick y eliminar espacio vacio en css

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const existingSearch = localStorage.getItem("search")
 
 existingSearch && ($searchText.textContent = existingSearch)
 
+const editName = (search) => {
+    $searchText.textContent = search
+    localStorage.setItem("search", search)
+}
+
 $formSearch.addEventListener("submit", e => {
     e.preventDefault()
     const search = document.getElementById("search-input").value
-    const editName = (search) => {
-        $searchText.textContent = search
-        localStorage.setItem("search", search)
-    }
     editSearch(":(")
     search.trim() && editSearch(search)
 })
@@ -18,6 +19,6 @@ $formSearch.addEventListener("submit", e => {
 $searchText.addEventListener("click", e => {
     const element = e.target
     if(element.tagSearch==="SPAN"){
-        element.innerHTML = '<input type="text" id="search-input" value="${element.textContent}">'
+        element.innerHTML = `<input type="text" id="search-input" value="${element.textContent}">`
     }
 })
